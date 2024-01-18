69c4628bf2ebe266238e5c0db1e318ec6fde5c03
Ujjawal <ujjawal.sharma@preplaced.in>
2024-01-18 17:14:58 +0530

    feat: added userinfo on page

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+const username = 'johnpapa';
+const accessToken = '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';
+
+function fetchUser(username) {
+  fetch(`https://api.github.com/users/${username}`, {
+    headers: {
+      Authorization: `token ${accessToken}`
+    }
+  })
+    .then(response => response.json())
+    .then(user => {
+      const profilePic = `<img src="${user.avatar_url}" alt="Profile Picture">`;
+      document.querySelector('.user-profile').innerHTML = profilePic;
+      document.querySelector('.username').innerHTML = `${user.name}`;
+      document.querySelector('.repo').innerHTML = `${user.html_url}`;
+      if(user.bio)
+      document.querySelector('.bio').innerHTML = `${user.bio}`;
+    if(user.location)
+    document.querySelector('.location').innerHTML = `${user.location}`;
+    if(user.twitter_username)
+      document.querySelector('.twitter').innerHTML = `${user.twitter_username}`;
+    })
+    .catch(error => {
+      console.error('Error fetching user data:', error);
+    });
+}
+fetchUser(username);
