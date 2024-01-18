ba9be74603ec5401b4a4dac9ae3329ab4856a2b6
Ujjawal <ujjawal.sharma@preplaced.in>
2024-01-19 00:02:08 +0530

    feat: added nextpage prevpage buttons

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 const username = 'johnpapa';
 const accessToken = '';
 let perPage = 10;
 let currentPage = 2;
+let numberofpages;
+
 function fetchUser(username) {
-  fetch(`https://api.github.com/users/${username}`)
+  fetch(`https://api.github.com/users/${username}`, {
+    headers: {
+      Authorization: `token ${accessToken}`
+    }
+  })
     .then(response => response.json())
     .then(user => {
+      numberofpages = Math.ceil(user.public_repos/perPage);
+      console.log(numberofpages);
       const profilePic = `<img src="${user.avatar_url}" alt="Profile Picture">`;
       const locationicon = `<img src="${user.avatar_url}" alt="Profile Picture">`;
       document.querySelector('.user-profile').innerHTML = profilePic;
@@ -19,7 +29,7 @@ function fetchUser(username) {
       console.error('Error fetching user data:', error);
     });
 }
-// fetchUser(username);
+fetchUser(username);
 function fetchRepositories(username, page) {
   fetch(`https://api.github.com/users/${username}/repos?page=${page}&per_page=${perPage}`, {
     headers: {
@@ -27,15 +37,16 @@ function fetchRepositories(username, page) {
     }
   })
     .then(response => response.json())
-    .then(data => {
-      console.log(data);
+    .then(data => {      
       const repos = data.map((repo)=>{
-        const topics = repo.topics.length > 0 ? `<p>Topics: ${repo.topics.join(', ')}</p>` : '';
+        const topics = repo.topics.length > 0 ? repo.topics.map((topic)=>{
+          return `<div class="tcard">${topic}</div>`
+        }).join('') : '';
         const desc= repo.description ? `<p>${repo.description}</p>` : '';
             return `<div class="card flex-col">
                       <h3 class="textblue" >${repo.name}</h3>
                       ${desc}
-                      ${topics}
+                      ${topics? `<div class="twrapper flex">${topics}</div>` :''}
                     </div>`;
       }).join('');
       console.log(repos);
@@ -46,52 +57,18 @@ function fetchRepositories(username, page) {
     });
 }
 fetchRepositories(username, currentPage);
-const user = {
-  "login": "johnpapa",
-  "id": 1202528,
-  "node_id": "MDQ6VXNlcjEyMDI1Mjg=",
-  "avatar_url": "https://avatars.githubusercontent.com/u/1202528?v=4",
-  "gravatar_id": "",
-  "url": "https://api.github.com/users/johnpapa",
-  "html_url": "https://github.com/johnpapa",
-  "followers_url": "https://api.github.com/users/johnpapa/followers",
-  "following_url": "https://api.github.com/users/johnpapa/following{/other_user}",
-  "gists_url": "https://api.github.com/users/johnpapa/gists{/gist_id}",
-  "starred_url": "https://api.github.com/users/johnpapa/starred{/owner}{/repo}",
-  "subscriptions_url": "https://api.github.com/users/johnpapa/subscriptions",
-  "organizations_url": "https://api.github.com/users/johnpapa/orgs",
-  "repos_url": "https://api.github.com/users/johnpapa/repos",
-  "events_url": "https://api.github.com/users/johnpapa/events{/privacy}",
-  "received_events_url": "https://api.github.com/users/johnpapa/received_events",
-  "type": "User",
-  "site_admin": false,
-  "name": "John Papa",
-  "company": "JohnPapa.net, LLC",
-  "blog": "http://johnpapa.net",
-  "location": "Orlando, FL",
-  "email": null,
-  "hireable": null,
-  "bio": "Winter is Coming",
-  "twitter_username": "john_papa",
-  "public_repos": 144,
-  "public_gists": 58,
-  "followers": 15219,
-  "following": 1,
-  "created_at": "2011-11-17T17:05:03Z",
-  "updated_at": "2023-11-01T15:31:38Z"
-}
-document.addEventListener('DOMContentLoaded', () => {
-  const profilePic = `<img src="${user.avatar_url}" alt="Profile Picture">`;
-  const locationicon = `<img src="${user.avatar_url}" alt="Profile Picture">`;
-  document.querySelector('.user-profile').innerHTML = profilePic;
-  document.querySelector('.username').innerHTML = `${user.name}`;
-  document.querySelector('.repo').innerHTML = `<img src="./assets/link.png" class="licon" alt=loc><a href="${user.html_url}" class="textblack">${user.html_url}</a>`;
-  if (user.bio) document.querySelector('.bio').innerHTML = `${user.bio}`;
-  if (user.location) document.querySelector('.location').innerHTML = `<img src="./assets/location.png" class="licon" alt=loc>  ${user.location}`;
-  if (user.twitter_username) document.querySelector('.twitter').innerHTML = `Twitter: @${user.twitter_username}`;
+
+document.querySelector('.next').addEventListener('click', () => {
+  currentPage++;
+  console.log(currentPage);
+  fetchRepositories(username, currentPage);
 });
-// , {
-//   headers: {
-//     Authorization: `token ${accessToken}`
-//   }
-// }
+
+document.querySelector('.footer .prev').addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchRepositories(username, currentPage);
+  }
+});
+
+});
